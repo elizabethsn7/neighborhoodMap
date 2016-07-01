b0813875788c109df764bc84457862c880a7b13f
Elizabeth Kipp <liz@elizabethkipp.com>
2016-07-01 17:03:47 -0500

    begining to load NYTimes API key

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    
 
     // YOUR CODE GOES HERE!
     var street = $('#street').val();
@@ -19,12 +19,56 @@ function loadData() {
     var address = street + ', ' + city;
     console.log(address);
 
+    // load streetview
     var streetUrl = 'https://maps.googleapis.com/maps/api/streetview?&size=600x400&location=' + address + '';
     console.log(streetUrl);
 
+    //I got confused here because of the fact that the src is using the + sign on both sides 
+    //of the streetUrl
     $body.append('<img class="bgimg" src="' + streetUrl + '">');
     
+    //load nytimes
+    var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+    var nytApyKey = 'de86a3351556445e91e98a55763166a7';
+
+    //YOUR CODE GOES HERE!
+
+    $.getJSON('nytUrl', function(data) {
+        var articles = [];
+        var i = 0;
+        for (i=0, i<=9, i++);
+            
+
+        $("<ul/" , {
+            "class": "nytimes-articles",
+            html: items.join("")
+        }).appendTo("body");
+    });
+
+    $.ajax({
+        url: nytUrl,
+        method: 'GET',
+    })
+    .done(function(result) {
+        console.log(result);
+    })
+    .fail(function(err) {
+        throw err;
+    });
+
+    console.log(data);
+
     return false;
 };
 
 $('#form-container').submit(loadData);
+
+
+
+
+
+
+
+
+
+
